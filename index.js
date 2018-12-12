8f70a42e3e061b71ac464702f442ed5149f03af6
sham <sagabriel@up.edu.ph>
2018-12-13 00:43:05 +0800

    heyhye

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.get('/',function(req,res){
 	res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client',express.static(__dirname + '/client'));
+
 serv.listen(2000);
 console.log("Server started");
 
